7b72f84f597b040e63d9b7b87c9fc7632a96fc87
rafael moraes <rafaelsm003@gmail.com>
2022-10-17 17:31:33 -0300

    feat: ordena em todos os documentos os valores do array valoresNutricionais pelo campo percentual de forma decrescente

---
+++ challenges/desafio24.js
@@ -0,0 +1,18 @@
+// 24 - Ordene em todos os documentos os valores do array valoresNutricionais 
+// pelo campo percentual de forma decrescente
+
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      valoresNutricionais: {
+        $each: [],
+        $sort: {
+          percentual: -1,
+        },
+      },
+    },
+  },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, valoresNutricionais: 1 });
