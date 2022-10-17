ee67c7de978f5e9cc781495b7c5bf10296c06e3a
rafael moraes <rafaelsm003@gmail.com>
2022-10-17 14:43:54 -0300

    feat: retorna o lanche mais vendido, mostrando apenas o nome e a quantidade do lanche mais vendido

---
+++ challenges/desafio3.js
@@ -0,0 +1,4 @@
+// 3 - Retorne o lanche mais vendido, mostrando apenas 
+// o nome e a quantidade do lanche mais vendido
+
+db.produtos.find({}, { _id: 0, nome: 1, vendidos: 1 }).sort({ vendidos: -1 }).limit(1);
